dce878864c577851d3f0ef13f3660847129744bd
Andrew Hill <hill.andrew2014@gmail.com>
2016-04-01 16:47:14 -0500

    Andrew's test commit.  + Marlin_Activity

---
+++ app/src/main/java/com/example/hilla_000/hellogithub/Marlin_Activity.java
@@ -0,0 +1,16 @@
+package com.example.hilla_000.hellogithub;
+
+import android.util.Log;
+
+public class Marlin_Activity {
+
+    private static final String TAG = "Marlin";
+
+    public Marlin_Activity(){
+
+    }
+
+    public void Fish() {
+        Log.v(TAG, "YEAH TOAST!");
+    }
+}
